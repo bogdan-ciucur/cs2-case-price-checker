687875f0a8b5f0e2989446e1e72a830a83e08295
bogdan-ciucur <90422134+bogdan-ciucur@users.noreply.github.com>
2025-05-10 11:28:28 +0300

    added 1 more recoil case

--- cases.py
+++ cases.py
@@ -35,7 +35,7 @@ cases = {
     "Operation Breakout Weapon Case": 29,
     "Operation Phoenix Weapon Case": 16,
     "Falchion Case": 8,
-    "Recoil Case": 1,
+    "Recoil Case": 2,
     "Dreams & Nightmares Case": 1,
 }
 
