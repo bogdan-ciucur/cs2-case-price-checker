d74e2c3bc6874bca5e7de6cbd1b8c27e6e6b5b48
bogdan-ciucur <90422134+bogdan-ciucur@users.noreply.github.com>
2025-07-10 00:20:34 +0300

    added 1 fracture case

--- cases.py
+++ cases.py
@@ -15,7 +15,7 @@ def get_case_price(case_name):
     return None
 
 cases = {
-    "Fracture Case": 36,
+    "Fracture Case": 37,
     "Snakebite Case": 4,
     "Clutch Case": 13,
     "Prisma Case": 5,
