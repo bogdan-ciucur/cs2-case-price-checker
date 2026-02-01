a8e7adf3cf4ef52c6554600d7647faa2afe9c8a8
bogdan-ciucur <90422134+bogdan-ciucur@users.noreply.github.com>
2026-02-01 16:45:10 +0200

    added 1 revolution case

--- cases.py
+++ cases.py
@@ -60,7 +60,7 @@ cases = {
     "Recoil Case": 76,
     "Dreams & Nightmares Case": 5,
     "Kilowatt Case": 7,
-    "Revolution Case": 6,
+    "Revolution Case": 7,
     "Sealed Genesis Terminal": 4,
 }
 
